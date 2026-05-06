1ebb108d1d22fa22e33c6fc8839f241c1e682b53
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2026-05-06 20:54:34 +0200

    firmware: hello world working

--- stm-firmware/scale/Core/Src/app.c
+++ stm-firmware/scale/Core/Src/app.c
@@ -1,10 +1,17 @@
-#include "main.h"
 #include "adc.h"
 #include "gpio.h"
-#include "usart.h"
+#include "main.h"
 #include "stm32u0xx_hal.h"
+#include "usart.h"
+#include <stdio.h>
 
 void SystemClock_Config(void);
+
+// Redirect printf to UART1 by implementing __io_putchar
+int __io_putchar(int ch) {
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+  return ch;
+}
 
 /**
  * @brief  The application entry point.
@@ -22,35 +29,26 @@ int main(void) {
    */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC1_Init();
   MX_USART1_UART_Init();
   MX_USART3_UART_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
+  HAL_GPIO_WritePin(RadioEnable_GPIO_Port, RadioEnable_Pin, GPIO_PIN_SET);
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+  // HAL_GPIO_TogglePin(Led_GPIO_Port, Led_Pin);
   while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
     // Toggle LED every 500ms (1s period)
-    HAL_GPIO_TogglePin(Led_GPIO_Port, Led_Pin);
-    HAL_Delay(500);
+    HAL_GPIO_WritePin(Led_GPIO_Port, Led_Pin, GPIO_PIN_SET);
+    HAL_Delay(1000);
+    HAL_GPIO_WritePin(Led_GPIO_Port, Led_Pin, GPIO_PIN_RESET);
+    HAL_Delay(1000);
+    printf("Hello there!\r\n");
   }
   /* USER CODE END 3 */
 }
